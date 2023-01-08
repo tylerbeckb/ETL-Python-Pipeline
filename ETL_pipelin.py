b1047e3e6d1f01f90b469966c7d742117741e62e
Tyler Brown <118109189+tylerbeckb@users.noreply.github.com>
2023-01-08 15:30:31 +0000

    Extract function made which loads JSON file

--- ETL_pipelin.py
+++ ETL_pipelin.py
@@ -1,3 +1,18 @@
 import os
 import pandas as pd
 import json
+
+path = '/workspaces/ETL-Python-Pipeline/'
+
+def extract(filename):
+    try:
+        # Checks if the file given is a json file
+        if filename.endswith(".json"):
+            # Opens the json file
+            f = open(filename)
+            # Loads it into a variable
+            data = json.load(f)
+    except:
+        pass
+
+extract('data.json')
